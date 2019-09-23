1ffa2f4333605515638369ca0d036062f68570ed
Charlie Smurthwaite <charlie@atechmedia.com>
2019-09-23 14:17:43 +0100

    always return self

--- spec/specs/index_hint_spec.rb
+++ spec/specs/index_hint_spec.rb
@@ -11,8 +11,9 @@ describe SQB::Select do
       expect(query.to_sql).to eq 'SELECT `posts`.* FROM `posts` USE INDEX (`index_by_author`)'
     end
 
-    it "should be possible to chain two index hints" do
-      query.index_hint(:index_by_author).index_hint(:index_by_date)
+    it "should be possible add two index hints" do
+      query.index_hint(:index_by_author)
+      query.index_hint(:index_by_date)
       expect(query.to_sql).to eq 'SELECT `posts`.* FROM `posts` USE INDEX (`index_by_author`, `index_by_date`)'
     end
 
@@ -20,6 +21,11 @@ describe SQB::Select do
       query.index_hint(:index_by_author)
       query.no_index_hint!
       expect(query.to_sql).to eq 'SELECT `posts`.* FROM `posts`'
+    end
+
+    it "should allow all methods to be chained" do
+      query.index_hint(:index_by_date).no_index_hint!.index_hint(:index_by_author)
+      expect(query.to_sql).to eq 'SELECT `posts`.* FROM `posts` USE INDEX (`index_by_author`)'
     end
 
   end
