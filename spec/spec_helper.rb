8cb237997d3591afddad8fb61d10c717a56639ac
Adam Cooke <me@adamcooke.io>
2020-02-21 13:17:15 +0000

    don't truncate expectation output

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,10 +5,12 @@ RSpec.configure do |config|
   config.color = true
 
   config.expect_with :rspec do |expectations|
+    expectations.max_formatted_output_length = 1000000
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
 
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
   end
+
 end
